47fbc349167c61bc20a82f39941fe2e1131f874e
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2023-10-20 06:30:02 +0500

    Add the API endpoint

--- src/config/app.config/index.js
+++ src/config/app.config/index.js
@@ -1,14 +1,1 @@
-export const BASE_URL = 'http://localhost:1337/api/products'
-
-// useEffect(() => {
-    // const fetchData = async () => {
-        // try {
-            // const res = await fetch(BASE_URL);
-            // const data = res.json()
-            // return data;      
-        // } catch (error) {
-            // return error;
-        // }
-    //   }
-    //   const data = fetchData();
-//   }, [BASE_URL])
+export const BASE_URL = 'https://trailmarket.up.railway.app'
