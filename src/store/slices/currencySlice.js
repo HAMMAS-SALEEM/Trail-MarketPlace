3befc5e2b0e466f2822cde65ca133e5c1d4bbbe9
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2023-11-14 22:27:27 +0500

    Fix currency problem

--- src/store/slices/currencySlice.js
+++ src/store/slices/currencySlice.js
@@ -3,13 +3,13 @@ import { BASE_URL } from "../../config/app.config";
 
 // Acutal Information for BigQuery Sheet
 
-const url = 'https://script.google.com/macros/s/AKfycbw7fRS8-Rvl54Jp_Cha8UJ-p2Pm8xeNnzM2I0eZGhpT2BD_kK23OT3vxYS1imb0inE6ZQ/exec'
-const sheetsKey = 'c30cfe18-e86b-4ae5-b164-ebd1f00e008d'
+// const url = 'https://script.google.com/macros/s/AKfycbw7fRS8-Rvl54Jp_Cha8UJ-p2Pm8xeNnzM2I0eZGhpT2BD_kK23OT3vxYS1imb0inE6ZQ/exec'
+// const sheetsKey = 'c30cfe18-e86b-4ae5-b164-ebd1f00e008d'
 
 // Sample Information
 
-// const url = 'https://script.google.com/macros/s/AKfycbwATm_Pxwmy8YXuCu9DZZHSKb9f3FiqHsLb3sXKBRsxpImQK_0zKOZzo-5D5P4qxwuR/exec'
-// const sheetsKey = 'e46e87e7-3f35-4330-83bd-0bca053b14d1';
+const url = 'https://script.google.com/macros/s/AKfycbwATm_Pxwmy8YXuCu9DZZHSKb9f3FiqHsLb3sXKBRsxpImQK_0zKOZzo-5D5P4qxwuR/exec'
+const sheetsKey = 'e46e87e7-3f35-4330-83bd-0bca053b14d1';
 
 export const fetchCurrency = createAsyncThunk('fetch/currency', async (userId) => {
   const sheetUrl = `${url}?key=${sheetsKey}&graniteUserId=${userId}`;
@@ -54,7 +54,7 @@ export const fetchCurrency = createAsyncThunk('fetch/currency', async (userId) =
   const amountSpent = currency[1].data[0].attributes.amount_spent;
   const totalCurrency = currency[0].trails
   const spentCurrency = currency[1].data[0].attributes.amount_spent;
-  const calculatedTrails = totalCurrency - spentCurrency;
+  const calculatedTrails = Math.abs(totalCurrency) - Math.abs(spentCurrency);
   let trails = 0
   if(calculatedTrails > 0) {
     trails = calculatedTrails.toString();
