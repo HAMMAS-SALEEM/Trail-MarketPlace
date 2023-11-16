3cad69ac5bac267bde1903566c076470c30231b9
JGUsman007 <sajidnaseem379@gmail.com>
2023-11-16 16:49:36 +0500

    styling update

--- src/components/MoreButton.js
+++ src/components/MoreButton.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const MoreButton = ({arrowMore, handlePagination}) => {
   return (
     <button type="button" onClick={handlePagination} className="arrow-more-btn">
-      <span className="arrow-more-btn-text">More</span>
+      <span className="arrow-more-btn-text">MORE</span>
       <img src={arrowMore} alt={arrowMore} className="arrow-more-btn-icon" />
     </button>
   )
